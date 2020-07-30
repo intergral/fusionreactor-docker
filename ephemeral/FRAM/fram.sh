0abcdd1bf4dc37c4d20ad11242794e6b4842d4f9
Neil Wightman <neil_wightman@intergral.com>
2020-07-30 13:30:52 +0200

    Improve scripts to document each arg

--- ephemeral/FRAM/fram.sh
+++ ephemeral/FRAM/fram.sh
@@ -1,6 +1,21 @@
 #/bin/sh
 
+# Configure defaults if not set by -e when running docker.
 LICENSE=${LICENSE:='XXXX-XXXX-XXXX-XXXX-XXXX'}
 PASS=${PASS:='admin'}
+PORT=${PORT:='8087'}
+INSTANCE=${INSTANCE:='FRAM'}
 
-java -javaagent:/opt/fusionreactor/instance/FRAM/fusionreactor.jar=name=FRAM,address=:8087 -Dfrnopointcuts -Dfrstartupdelay=0 -Djava.awt.headless=true -Xmx512m -Dfr.ed.ds.enable=true -Dfrlicense=${LICENSE} -Dfradminpassword=${PASS} -cp /opt/fusionreactor/instance/FRAM/fusionreactor.jar com.intergral.fusionreactor.agent.service.Service
+# Add jar agent.
+export JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/fusionreactor/instance/FRAM/fusionreactor.jar=address=${PORT},name=${INSTANCE}"
+# Set license key (can be configured via -e LICENSE=).
+export JAVA_OPTS="$JAVA_OPTS -Dfrlicense=${LICENSE}"
+# Set admin password.
+export JAVA_OPTS="$JAVA_OPTS -Dfradminpassword=${PASS}"
+# Enable enterprise dashboard service.
+export JAVA_OPTS="$JAVA_OPTS -Dfr.ed.ds.enable=true"
+# Disable pointcut logic of normal FR agents and start with 0 delay.
+export JAVA_OPTS="$JAVA_OPTS -Dfrnopointcuts -Dfrstartupdelay=0"
+
+# Run the framd entry point
+java ${JAVA_OPTS} -Djava.awt.headless=true -Xmx512m -cp /opt/fusionreactor/instance/FRAM/fusionreactor.jar com.intergral.fusionreactor.agent.service.Service
--- ephemeral/Tomcat/runTomcat.sh
+++ ephemeral/Tomcat/runTomcat.sh
@@ -1,9 +1,24 @@
+#/bin/sh
+
+# Configure defaults if not set by -e when running docker.
 LICENSE=${LICENSE:='XXXX-XXXX-XXXX-XXXX-XXXX'}
 PASS=${PASS:='admin'}
 TARGET=${TARGET:='fram'}
 GROUPS=${GROUPS:='default'}
 INSTANCE=${INSTANCE:='tomcat'}
+PORT=${PORT:="8088"}
 
-export JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/fusionreactor/instance/tomcat/fusionreactor.jar=address=8088,name=${INSTANCE} -agentpath:/opt/fusionreactor/instance/tomcat/libfrjvmti_x64.so -Dfrlicense=${LICENSE} -Dfr.ed.ds.target=${TARGET}:2106 -Dfradminpassword=${PASS} -Dfr.ed.ds.groups=${GROUPS}"
+# Add jar agent
+export JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/fusionreactor/instance/tomcat/fusionreactor.jar=address=${PORT},name=${INSTANCE}"
+# Add native debug library
+export JAVA_OPTS="$JAVA_OPTS -agentpath:/opt/fusionreactor/instance/tomcat/libfrjvmti_x64.so"
+# Set license key (can be configured via -e LICENSE=)
+export JAVA_OPTS="$JAVA_OPTS -Dfrlicense=${LICENSE}"
+# Set the Enterprise Dashboard target
+export JAVA_OPTS="$JAVA_OPTS -Dfr.ed.ds.target=${TARGET}:2106"
+# Define Enterprise Dashboard group
+export JAVA_OPTS="$JAVA_OPTS -Dfr.ed.ds.groups=${GROUPS}"
+# Set admin password
+export JAVA_OPTS="$JAVA_OPTS -Dfradminpassword=${PASS}"
 
 /usr/local/tomcat/bin/catalina.sh run
